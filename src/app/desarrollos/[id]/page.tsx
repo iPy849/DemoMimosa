23efeb29665f1b43b14a9f8e4060739959fa2a19
Alejandro Ortega <AleOrtega849@gmail.com>
2026-02-10 11:24:23 +0000

    Try fixing this error: `Runtime ReferenceError: cn is not defined. Error

--- src/app/desarrollos/[id]/page.tsx
+++ src/app/desarrollos/[id]/page.tsx
@@ -14,6 +14,7 @@ import {
   type CarouselApi,
 } from "@/components/ui/carousel";
 import { useState, useEffect } from "react";
+import { cn } from "@/lib/utils";
 
 const developments = {
   "cuatro-bacalar": {
